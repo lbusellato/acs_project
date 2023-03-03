fe74261f8875e23fd6e87aec16824a5d352d37eb
lbusellato <lorenzo.busellato@gmail.com>
2023-03-03 10:44:10 +0100

    Assignments 14-15

--- assignments/assignment14.m
+++ assignments/assignment14.m
@@ -15,8 +15,23 @@ addpath(genpath('../common/'));
 addpath(genpath('../simulink/'));
 % Load the robot struct
 robotStruct;
+% Set the robot in the [0 -0.3 0] configuration
+robot.config(2).JointPosition = -0.3;
 
 %% PARAMETERS
+% PD control law
+KP = 50*ones(6,1);
+KD = 100*ones(6,1);
+Md = diag([0.5;0.5;0.5;1;1;1]);
+invMd = inv(Md);
+KI = 0*ones(1,6);
+KF = 10*ones(1,6);
+% Environment pose
+xr = k([0 -0.1 0]);
+% Environment stiffness
+K = 5*eye(6);
+% Desired force
+fd = [0 1 0 0 0 0]';
 
 %% SIMULINK 
 
